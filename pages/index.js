72893854372ae6578a4fc5e7f358b88d63e291db
Tosei <359387050@qq.com>
2022-10-04 12:12:19 +0900

    fix: update contents

--- pages/index.js
+++ pages/index.js
@@ -64,22 +64,22 @@ export default function Home() {
           <div className="mt-5">
             <h1
               ref={textOne}
-              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-4/5 mob:w-full laptop:w-4/5">
+              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-3xl p-1 tablet:p-2 text-bold w-4/5 mob:w-full laptop:w-4/5">
               {data.headerTaglineOne}
             </h1>
             <h1
               ref={textTwo}
-              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-6xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5">
+              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-2xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5">
               {data.headerTaglineTwo}
             </h1>
             <h1
               ref={textThree}
-              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-6xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5">
+              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-2xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5">
               {data.headerTaglineThree}
             </h1>
             <h1
               ref={textFour}
-              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5">
+              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-2xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5">
               {data.headerTaglineFour}
             </h1>
           </div>
